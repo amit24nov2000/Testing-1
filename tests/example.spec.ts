85fd9ef3b88fd1a0871d2803f83c6729e356068a
amit24nov2000 <amit24nov2000@gmail.com>
2024-07-08 10:25:51 +0530

    commiting

---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://artoftesting.com/samplesiteforselenium');
+  var dropdown:string='Manual Testing';
+  //await page.locator('//*[@id="testingDropdown"]').click();
+  await page.locator('#testingDropdown').selectOption({ label: 'Manual Testing' });
+  //In order to select the select option 
+  await page.waitForTimeout(5000);
+});
